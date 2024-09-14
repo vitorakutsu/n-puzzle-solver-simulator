d3973de545e4f5393b258c8aab5951dd871911e4
vitorakutsu <vitorakutsu@gmail.com>
2024-09-13 22:21:37 -0300

    feat: added images

---
+++ src/assets/images-mapping.ts
@@ -0,0 +1,5 @@
+import LogoImage from './images/logo.png';
+import BrazilImage from './images/brazil.png';
+import UnitedStateImage from './images/usa.png';
+
+export { LogoImage, BrazilImage, UnitedStateImage };
