9b3c44ee7ce303ebd66964d211c929c3f02c02b6
vitorakutsu <vitorakutsu@gmail.com>
2024-09-13 22:22:23 -0300

    feat: added themes

---
+++ src/styles/theme/color.ts
@@ -0,0 +1,12 @@
+export const color = {
+  primary: '#C6C2F2',
+  secondary: '#D6DC82',
+  tertiary: '#D86072',
+  background: '#FFF6EE',
+  neutral: {
+    lightest: '#F9FAFF',
+    light: '#E1E1E1',
+    dark: '#CCCCCC',
+    darkest: '#262628',
+  },
+};
---
+++ src/styles/theme/font-size.ts
@@ -0,0 +1,9 @@
+export const fontSize = {
+  xxs: '12px',
+  xs: '16px',
+  s: '18px',
+  m: '24px',
+  l: '32px',
+  xl: '48px',
+  xxl: '64px',
+};
