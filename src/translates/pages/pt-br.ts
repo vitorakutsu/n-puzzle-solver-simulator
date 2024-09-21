db1aa94aa063e48038dc8ebbb5783a492ff03c79
vitorakutsu <vitorakutsu@gmail.com>
2024-09-21 17:00:05 -0300

    feat: added translates

--- src/translates/pages/pt-br.ts
+++ src/translates/pages/pt-br.ts
@@ -9,5 +9,62 @@ export const ptBrPageTranslations = {
         start: 'Começar',
       },
     },
+    howItWorks: {
+      title: 'NPuzzle Solver',
+      subtitle: 'Como funciona?',
+      steps: [
+        {
+          title: '1. Escolha do Estado Inicial e Final:',
+          text: [
+            {
+              paragraph:
+                '• O usuário pode definir o estado inicial do NPuzzle, ou seja, a disposição das peças no início do jogo.',
+            },
+            {
+              paragraph:
+                '• O estado final, que representa o objetivo, também pode ser configurado. Geralmente, o estado final corresponde às peças organizadas em ordem numérica, mas o usuário pode personalizá-lo.',
+            },
+          ],
+        },
+        {
+          title: '2. Seleção do Algoritmo Heurístico:',
+          text: [
+            {
+              paragraph:
+                '• A aplicação oferece diferentes algoritmos de busca heurística para resolver o quebra-cabeça.',
+            },
+            {
+              paragraph:
+                '• O usuário pode selecionar o algoritmo desejado, cada um com sua própria estratégia para encontrar a solução mais eficiente.',
+            },
+          ],
+        },
+        {
+          title: '3. Início da Simulação:',
+          text: [
+            {
+              paragraph:
+                '• Ao iniciar a simulação, o algoritmo começa a explorar os possíveis movimentos, buscando uma solução que leve o estado inicial ao estado final.',
+            },
+            {
+              paragraph:
+                '• A interface exibe visualmente cada etapa do processo, permitindo ao usuário acompanhar como o algoritmo navega entre diferentes estados do puzzle.',
+            },
+          ],
+        },
+        {
+          title: '4. Visualização do Processo de Resolução:',
+          text: [
+            {
+              paragraph:
+                '• A aplicação mostra a sequência de movimentos que o algoritmo faz para resolver o puzzle, como quais peças são movidas e em que direção.',
+            },
+          ],
+        },
+      ],
+      button: {
+        start: 'Começar',
+      },
+    },
   },
 };
