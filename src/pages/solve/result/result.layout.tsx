43b10cd8e6bbb93b905613c55e8127f70f1a2c60
vitorakutsu <vitorakutsu@gmail.com>
2024-09-22 17:16:46 -0300

    feat: added result page

--- src/pages/solve/result/result.layout.tsx
+++ src/pages/solve/result/result.layout.tsx
@@ -1,13 +1,73 @@
-import { SolveSteps } from '../types';
+import { State } from '~/components/state/state';
+import { useTranslation } from '~/translates/use-translation';
+import {
+  ButtonWrapper,
+  Column,
+  Info,
+  InfoTitle,
+  InfoWrapper,
+  Label,
+  SecondaryButton,
+  TertiaryButton,
+  TextWrapper,
+  Title,
+  Value,
+} from './result.styles';
+import { IInfoState, SolveSteps } from '../types';
 
 interface ISolveResultLayout {
+  state: number[];
+  info: IInfoState;
   onChangeStep: (step: SolveSteps) => void;
+  onStepByStep: () => void;
 }
 
-export const SolveResultLayout = ({}: ISolveResultLayout) => {
+export const SolveResultLayout = ({ state, info, onChangeStep, onStepByStep }: ISolveResultLayout) => {
+  const translate = useTranslation('pages.result');
+
+  const goToStart = () => onChangeStep(SolveSteps.FINAL_STATE);
+
+  const renderText = () => (
+    <TextWrapper>
+      <Title>{translate('title')}</Title>
+    </TextWrapper>
+  );
+
+  const renderInfo = (type: keyof IInfoState) => (
+    <Info>
+      <Label>{translate(`info.${type}`)}</Label>
+      <Value>{info[type]}</Value>
+    </Info>
+  );
+
+  const renderInfoSection = () => (
+    <Column>
+      <InfoTitle>{translate('info.title')}</InfoTitle>
+      {renderInfo('algorithm')}
+      {renderInfo('steps')}
+      {renderInfo('time')}
+    </Column>
+  );
+
+  const renderButton = () => (
+    <ButtonWrapper>
+      <SecondaryButton label={translate('button.stepByStep')} onClick={onStepByStep} />
+      <TertiaryButton label={translate('button.tryAgain')} onClick={goToStart} />
+    </ButtonWrapper>
+  );
+
+  const renderContent = () => (
+    <InfoWrapper>
+      {renderText()}
+      {renderInfoSection()}
+      {renderButton()}
+    </InfoWrapper>
+  );
+
   return (
-    <div>
-      <h1>SolveResult</h1>
-    </div>
+    <>
+      <State puzzleState={state} />
+      {renderContent()}
+    </>
   );
 };
--- src/pages/solve/result/result.styles.ts
+++ src/pages/solve/result/result.styles.ts
@@ -26,25 +26,42 @@ export const Title = styled.p`
   font-size: ${fontSize.xl};
 `;
 
-export const Paragraph = styled.p`
-  font-family: regular;
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const InfoTitle = styled.p`
+  font-family: semi-bold;
   font-size: ${fontSize.m};
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const Label = styled.p`
+  font-family: semi-bold;
+  font-size: ${fontSize.s};
+`;
+
+export const Value = styled.p`
+  font-family: regular;
+  font-size: ${fontSize.s};
 `;
 
 export const ButtonWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
   gap: 16px;
-  margin-top: 16px;
 `;
 
-export const Subtitle = styled.p`
-  font-family: semi-bold;
-  font-size: ${fontSize.l};
-`;
-
-export const PrimaryButton = styled(Button).attrs({
-  type: ButtonType.PRIMARY,
+export const TertiaryButton = styled(Button).attrs({
+  type: ButtonType.TERTIARY,
 })``;
 
 export const SecondaryButton = styled(Button).attrs({
