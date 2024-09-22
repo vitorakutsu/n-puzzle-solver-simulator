d275e2a8065f409d3a53d87005fdff565d4d52b0
vitorakutsu <vitorakutsu@gmail.com>
2024-09-22 17:16:53 -0300

    feat: added new colors

--- src/styles/theme/color.ts
+++ src/styles/theme/color.ts
@@ -1,7 +1,10 @@
 export const color = {
   primary: '#C6C2F2',
+  darkPrimary: '#B6B2E2',
   secondary: '#D6DC82',
+  darkSecondary: '#C6CC72',
   tertiary: '#D86072',
+  darkTertiary: '#C85062',
   background: '#FFF6EE',
   neutral: {
     lightest: '#F9FAFF',
