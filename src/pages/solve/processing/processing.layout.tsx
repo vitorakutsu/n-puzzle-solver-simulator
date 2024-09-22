cc51d070998b3a6a81bede60b7a47499fbcdf707
vitorakutsu <vitorakutsu@gmail.com>
2024-09-22 17:16:39 -0300

    feat: added processing page

--- src/pages/solve/processing/processing.layout.tsx
+++ src/pages/solve/processing/processing.layout.tsx
@@ -1,14 +1,35 @@
-
-import { SolveSteps } from '../types';
+import { State } from '~/components/state/state';
+import { useTranslation } from '~/translates/use-translation';
+import { InfoWrapper, Loader, TextWrapper, Title } from './processing.styles';
 
 interface ISolveProcessingLayout {
-  onChangeStep: (step: SolveSteps) => void;
+  state: number[];
 }
 
-export const SolveProcessingLayout = ({}: ISolveProcessingLayout) => {
+export const SolveProcessingLayout = ({ state }: ISolveProcessingLayout) => {
+  const translate = useTranslation('pages.processing');
+
+  const renderText = () => (
+    <TextWrapper>
+      <Title>{translate('title')}</Title>
+      {renderLoader()}
+    </TextWrapper>
+  );
+
+  const renderLoader = () => (
+    <Loader>
+      <span></span>
+      <span></span>
+      <span></span>
+    </Loader>
+  );
+
+  const renderInfo = () => <InfoWrapper>{renderText()}</InfoWrapper>;
+
   return (
-    <div>
-      <h1>SolveResult</h1>
-    </div>
+    <>
+      <State puzzleState={state} />
+      {renderInfo()}
+    </>
   );
 };
--- src/pages/solve/processing/processing.styles.ts
+++ src/pages/solve/processing/processing.styles.ts
@@ -1,24 +1,20 @@
 import styled from 'styled-components';
-import { Button, ButtonType } from '~/components/button/button';
+import { color } from '~/styles/theme/color';
 import { fontSize } from '~/styles/theme/font-size';
 
-export const Container = styled.div`
+export const InfoWrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-`;
-
-export const InfoWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  justify-content: center;
   flex: 1;
   gap: 80px;
 `;
 
 export const TextWrapper = styled.div`
   text-align: center;
-  flex-direction: column;
-  gap: 16px;
+  flex-direction: row;
+  gap: 8px;
 `;
 
 export const Title = styled.p`
@@ -26,27 +22,36 @@ export const Title = styled.p`
   font-size: ${fontSize.xl};
 `;
 
-export const Paragraph = styled.p`
-  font-family: regular;
-  font-size: ${fontSize.m};
-`;
-
-export const ButtonWrapper = styled.div`
+export const Loader = styled.div`
   display: flex;
-  justify-content: space-around;
-  gap: 16px;
-  margin-top: 16px;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+
+  span {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    margin: 0 5px;
+    border-radius: 50%;
+    background-color: ${color.neutral.darkest};
+    animation: bounce 0.6s infinite alternate;
+  }
+
+  span:nth-child(2) {
+    animation-delay: 0.2s;
+  }
+
+  span:nth-child(3) {
+    animation-delay: 0.4s;
+  }
+
+  @keyframes bounce {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-20px);
+    }
+  }
 `;
-
-export const Subtitle = styled.p`
-  font-family: semi-bold;
-  font-size: ${fontSize.l};
-`;
-
-export const PrimaryButton = styled(Button).attrs({
-  type: ButtonType.PRIMARY,
-})``;
-
-export const SecondaryButton = styled(Button).attrs({
-  type: ButtonType.SECONDARY,
-})``;
