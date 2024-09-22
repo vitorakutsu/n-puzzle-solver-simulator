fe049ae3413494a029e46d7f6bd843c0e47402d2
vitorakutsu <vitorakutsu@gmail.com>
2024-09-21 23:14:46 -0300

    feat: added translates

--- src/translates/components/en-us.ts
+++ src/translates/components/en-us.ts
@@ -1,11 +1,3 @@
 export const enUsComponentTranslations = {
-  components: {
-    topBar: {
-      options: {
-        start: 'Start',
-        howItWorks: 'How it works',
-        about: 'About',
-      },
-    },
-  },
+  components: {},
 };
--- src/translates/pages/pt-br.ts
+++ src/translates/pages/pt-br.ts
@@ -1,43 +1,54 @@
-import { start } from "repl";
-
 export const ptBrPageTranslations = {
   pages: {
+    home: {
+      title: 'NPuzzle Solver',
+      paragraph:
+        'Esta aplicação em ReactJS simula a resolução do NPuzzle utilizando algoritmos de busca heurística. O usuário pode configurar livremente tanto o estado inicial quanto o estado final do quebra-cabeça, permitindo personalizar as posições das peças. A interface visualiza o processo de resolução em tempo real, mostrando como o algoritmo navega entre diferentes estados para encontrar a solução. Ideal para entender o funcionamento de algoritmos de busca heurística de forma interativa e dinâmica.',
+      button: {
+        howItWorks: 'Como funciona?',
+        start: 'Começar',
+      },
+    },
     about: {
       title: 'NPuzzle Solver',
       subtitle: 'Sobre',
-      paragraph: 'A aplicação NPuzzle Solver em ReactJS foi desenvolvida para simular a resolução do NPuzzle utilizando algoritmos de busca heurística. Ela permite que o usuário personalize o estado inicial e final do quebra-cabeça, escolhendo diferentes heurísticas para resolver o problema e visualizar o processo de busca em tempo real. Criada com o objetivo de fornecer uma ferramenta educacional interativa, a aplicação foi construída com ReactJS e outras bibliotecas para otimizar a visualização e o cálculo das soluções. Feito por Vitor Eyki Akutsu e Pedro Correia Felitto, ambos do Sexto Termo de Bacharelado em Ciência da Unoeste.',
+      paragraph:
+        'A aplicação NPuzzle Solver em ReactJS foi desenvolvida para simular a resolução do NPuzzle utilizando algoritmos de busca heurística. Ela permite que o usuário personalize o estado inicial e final do quebra-cabeça, escolhendo diferentes heurísticas para resolver o problema e visualizar o processo de busca em tempo real. Criada com o objetivo de fornecer uma ferramenta educacional interativa, a aplicação foi construída com ReactJS e outras bibliotecas para otimizar a visualização e o cálculo das soluções. Feito por Vitor Eyki Akutsu e Pedro Correia Felitto, ambos do Sexto Termo de Bacharelado em Ciência da Unoeste.',
       button: {
         start: 'Começar',
-      }
+      },
     },
-    finalStateChoice: {
+    finalState: {
       title: 'Escolha o estado final',
       subtitle: 'Vamos começar',
-      paragraph: 'Você precisará definir o estado final do NPuzzle. Esse estado representa o objetivo que o algoritmo tentará alcançar, ou seja, a disposição correta das peças no tabuleiro. Você poderá arrastar e soltar as peças para organizar o puzzle da forma que deseja ou escolher entre algumas configurações pré-definidas. Este será o layout que o algoritmo terá como referência ao tentar resolver o puzzle.',
+      paragraph:
+        'Você precisará definir o estado final do NPuzzle. Esse estado representa o objetivo que o algoritmo tentará alcançar, ou seja, a disposição correta das peças no tabuleiro. Você poderá arrastar e soltar as peças para organizar o puzzle da forma que deseja ou escolher entre algumas configurações pré-definidas. Este será o layout que o algoritmo terá como referência ao tentar resolver o puzzle.',
       button: {
         shuffle: 'Embaralhar',
         next: 'Próximo',
-      }
+      },
     },
-    startStateChoice: {
+    initialState: {
       title: 'Escolha o estado inicial',
       subtitle: 'Vamos começar',
-      paragraph: 'Após definir o estado final, você deve configurar o estado inicial do NPuzzle. Aqui, você monta a configuração inicial do quebra-cabeça, que é o ponto de partida para o algoritmo. Assim como na seleção do estado final, você poderá mover as peças para escolher como elas estarão dispostas no início da simulação. É a partir dessa configuração que o algoritmo começará a busca pela solução, realizando os movimentos necessários até atingir o estado final.',
+      paragraph:
+        'Após definir o estado final, você deve configurar o estado inicial do NPuzzle. Aqui, você monta a configuração inicial do quebra-cabeça, que é o ponto de partida para o algoritmo. Assim como na seleção do estado final, você poderá mover as peças para escolher como elas estarão dispostas no início da simulação. É a partir dessa configuração que o algoritmo começará a busca pela solução, realizando os movimentos necessários até atingir o estado final.',
       button: {
         shuffle: 'Embaralhar',
         back: 'Voltar',
         next: 'Próximo',
-      }
+      },
     },
-    algorithmChoice: {
+    algorithm: {
       title: 'Escolha o algoritmo',
       subtitle: 'Vamos começar',
-      paragraph: 'Com o estado final e o estado inicial definidos,  você poderá escolher entre diferentes tipos de algoritmos heurísticos, cada um com uma abordagem única para resolver o NPuzzle. Por exemplo, alguns algoritmos podem priorizar a solução mais rápida, enquanto outros focam em explorar diferentes caminhos de maneira eficiente. Você poderá selecionar o algoritmo que deseja testar para visualizar a resolução do puzzle.',
+      paragraph:
+        'Com o estado final e o estado inicial definidos,  você poderá escolher entre diferentes tipos de algoritmos heurísticos, cada um com uma abordagem única para resolver o NPuzzle. Por exemplo, alguns algoritmos podem priorizar a solução mais rápida, enquanto outros focam em explorar diferentes caminhos de maneira eficiente. Você poderá selecionar o algoritmo que deseja testar para visualizar a resolução do puzzle.',
       button: {
         chooseAnAlgorithm: 'Escolher um algoritmo',
         back: 'Voltar',
         next: 'Próximo',
-      }
+      },
     },
     howItWorks: {
       title: 'NPuzzle Solver',
